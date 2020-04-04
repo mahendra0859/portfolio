a6064c4633679466f20f28d6f077848fc329edcd
Mahendra AR <mahendra0859@gmail.com>
2020-04-05 02:37:34 +0530

    html resume added

--- js/custom.js
+++ js/custom.js
@@ -1,30 +1,24 @@
+$("#yellow-color").click(function (e) {
+  $(".main-wrapper-resume").attr("id", "yellow");
+});
+$("#red-color").click(function (e) {
+  $(".main-wrapper-resume").attr("id", "red");
+});
+$("#blue-color").click(function (e) {
+  $(".main-wrapper-resume").attr("id", "blue");
+});
+$("#green-color").click(function (e) {
+  $(".main-wrapper-resume").attr("id", "green");
+});
 
-
- $('#yellow-color').click(function(e){
-             $(".main-wrapper-resume").attr("id", "yellow");
-         });
-         $('#red-color').click(function(e){
-             $(".main-wrapper-resume").attr("id", "red");
-         });
-         $('#blue-color').click(function(e){
-             $(".main-wrapper-resume").attr("id", "blue");
-         });
-         $('#green-color').click(function(e){
-             $(".main-wrapper-resume").attr("id", "green");
-         });
-		 
-$(document).ready(function(){
-  $(".setting-icon").click(function(){
+$(document).ready(function () {
+  $(".setting-icon").click(function () {
     $(".color-box").toggleClass("main");
   });
 });
 
-
-$(document).ready(function(){
-  $(".socials-text").click(function(){
+$(document).ready(function () {
+  $(".socials-text").click(function () {
     $(".socialicons").toggleClass("main");
   });
 });
-
-
-
