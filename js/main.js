10dfb229d253e9dec52f84de79a3fb17ad15a342
Mahendra AR <mahendra0859@gmail.com>
2020-04-13 13:45:37 +0530

    loader added

--- js/main.js
+++ js/main.js
@@ -1,179 +1,187 @@
-jQuery(document).ready(function(){ 
-	
-	/* ---------------------------------------------------------------------- */
-	/*	Custom Functions
+jQuery(document).ready(function () {
+  /* ---------------------------------------------------------------------- */
+  /*	Custom Functions
 	/* ---------------------------------------------------------------------- */
 
-	// Logo
-	var $logo 	= $('#logo');
+  // Logo
+  var $logo = $("#logo");
 
-    if (location.href.indexOf("#") != -1) {
-        $logo.show();
+  if (location.href.indexOf("#") != -1) {
+    $logo.show();
+  }
+  // Show logo
+  $(".menu .tabs a").click(function () {
+    $logo.fadeIn("slow");
+  });
+  // Hide logo
+  $(".tab-profile").click(function () {
+    $logo.fadeOut("slow");
+  });
+
+  /* ---------------------------------------------------------------------- */
+  /*	Resume
+	/* ---------------------------------------------------------------------- */
+
+  // Rating bars
+  $(".skills li .rating").each(function (index, e) {
+    // Vars
+    var $ratNum = 7,
+      $rat = $(e).attr("data-rat"),
+      $point = "<span></span>";
+
+    // Append points
+    while ($ratNum > 0) {
+      $(e).append($point);
+      $ratNum--;
     }
-	// Show logo 
-	$('.menu .tabs a').click(function() {
-	  $logo.fadeIn('slow');
-	});
-	// Hide logo
-	$('.tab-profile').click(function() {
-	  $logo.fadeOut('slow');
-	});	
 
-    
+    $(e)
+      .find("span")
+      .each(function (index, e) {
+        if (index >= $rat) return false;
+        // Append Disabled Rats
+        $(e).animate({
+          opacity: 1,
+        });
+      });
+  });
+
+  /* ---------------------------------------------------------------------- */
+  /*	About
 	/* ---------------------------------------------------------------------- */
-	/*	Resume
+
+  // Profile Photo Slider
+  $(".photo-inner ul").carouFredSel({
+    direction: "left",
+    circular: true,
+    auto: true,
+    scroll: {
+      items: 1,
+      fx: "crossfade",
+      duration: 1500,
+      wipe: true,
+    },
+    swipe: {
+      onTouch: true,
+    },
+    items: {
+      width: 153,
+    },
+  });
+
+  /* ---------------------------------------------------------------------- */
+  /*	Menu
 	/* ---------------------------------------------------------------------- */
-	
-	// Rating bars
-	$(".skills li .rating").each(function(index,e) { 
 
-		// Vars
-		var 
-			$ratNum = 7,
-			$rat = $(e).attr("data-rat"),
-			$point = "<span></span>";
+  // Needed variables
+  var $content = $("#content");
 
-		// Append points
-		while($ratNum > 0){
-		     $(e).append($point);
-		     $ratNum--;
-		}
+  // Run easytabs
+  $content.easytabs({
+    animate: true,
+    updateHash: false,
+    transitionIn: "slideDown",
+    transitionOut: "slideUp",
+    animationSpeed: 600,
+    tabs: ".tmenu",
+    tabActiveClass: "active",
+  });
 
-		$(e).find("span").each(function(index,e) { 
-			if(index >= $rat) return false;
-			// Append Disabled Rats
-			$(e).animate({
-			    opacity: 1
-			  });
-		});
+  // Hover menu effect
+  $content.find(".tabs li a").hover(
+    function () {
+      $(this).stop().animate({ marginTop: "-7px" }, 200);
+    },
+    function () {
+      $(this).stop().animate({ marginTop: "0px" }, 300);
+    }
+  );
 
+  // Menu Navigation
+  $(".menu .tabs").carouFredSel({
+    responsive: true,
+    direction: "left",
+    circular: false,
+    infinite: false,
+    pagination: "#menu-controls",
+    auto: false,
+    scroll: {
+      items: 1,
+      duration: 300,
+      wipe: true,
+    },
+    prev: {
+      button: "#menu-prev",
+      key: "right",
+    },
+    next: {
+      button: "#menu-next",
+      key: "left",
+    },
+    swipe: {
+      onTouch: true,
+    },
+    items: {
+      width: 140,
+      visible: {
+        min: 2,
+        max: 5,
+      },
+    },
+  });
+  /* ---------------------------------------------------------------------- */
+  /*	Cats Filter
+	/* ---------------------------------------------------------------------- */
+
+  var $catsfilter = $(".cats-filter");
+
+  // Copy categories to item classes
+  $catsfilter.find("a").click(function () {
+    var currentOption = $(this).attr("data-filter");
+    $(this).parent().parent().find("a").removeClass("current");
+    $(this).addClass("current");
+  });
+
+  /* ---------------------------------------------------------------------- */
+  /*	Portfolio
+	/* ---------------------------------------------------------------------- */
+
+  // Needed variables
+  var $plist = $("#portfolio-list");
+  var $pfilter = $("#portfolio-filter");
+
+  // Run Isotope
+  $plist.isotope({
+    filter: "*",
+    layoutMode: "masonry",
+    animationOptions: {
+      duration: 750,
+      easing: "linear",
+    },
+  });
+
+  // Isotope Filter
+  $pfilter.find("a").click(function () {
+    var selector = $(this).attr("data-filter");
+    $plist.isotope({
+      filter: selector,
+      animationOptions: {
+        duration: 750,
+        easing: "linear",
+        queue: false,
+      },
     });
+    return false;
+  });
+});
 
-	/* ---------------------------------------------------------------------- */
-	/*	About
-	/* ---------------------------------------------------------------------- */
-	
-	// Profile Photo Slider
-	 $(".photo-inner ul").carouFredSel({
-        direction           : "left",
- 	    circular: true,
-        auto    			: true,
-        scroll 			: {
-            items           : 1,
-            fx 				: 'crossfade',
-            duration        : 1500,                        
-            wipe    		: true
-        },
-	    swipe: {
-	        onTouch: true
-	    },
-        items: {
-            width: 153
-        }           
-    });
-	 
-	/* ---------------------------------------------------------------------- */
-	/*	Menu
-	/* ---------------------------------------------------------------------- */
-	
-	// Needed variables
-	var $content 		= $("#content");
-	
-	// Run easytabs
-  	$content.easytabs({
-	  animate			: true,
-	  updateHash		: false,
-	  transitionIn		:'slideDown',
-	  transitionOut		:'slideUp',
-	  animationSpeed	:600,
-	  tabs				:".tmenu",
-	  tabActiveClass	:'active',
-	});
-
-	
-	// Hover menu effect
-	$content.find('.tabs li a').hover(
-		function() {
-			$(this).stop().animate({ marginTop: "-7px" }, 200);
-		},function(){
-			$(this).stop().animate({ marginTop: "0px" }, 300);
-		}
-	);
-
-	// Menu Navigation
-	 $(".menu .tabs").carouFredSel({
-        responsive          : true,
-        direction           : "left",
- 	    circular: false,
-    	infinite: false,
-        pagination  		: "#menu-controls",  
-        auto    			: false,
-        scroll 			: {
-            items           : 1,
-            duration        : 300,                        
-            wipe    : true
-        },
-		prev	: {	
-			button	: "#menu-prev",
-			key		: "right"
-		},
-		next	: { 
-			button	: "#menu-next",
-			key		: "left"
-		},
-	    swipe: {
-	        onTouch: true
-	    },
-        items: {
-            width: 140,
-            visible: {
-              min: 2,
-              max: 5
-            }
-        }           
-    });
-	/* ---------------------------------------------------------------------- */
-	/*	Cats Filter
-	/* ---------------------------------------------------------------------- */ 
-	
-	var $catsfilter 		= $('.cats-filter');
-
-	// Copy categories to item classes
-	$catsfilter.find('a').click(function() {
-		var currentOption = $(this).attr('data-filter');
-		$(this).parent().parent().find('a').removeClass('current');
-		$(this).addClass('current');
-	});	
-
-	/* ---------------------------------------------------------------------- */
-	/*	Portfolio
-	/* ---------------------------------------------------------------------- */ 
-	
-	// Needed variables
-	var $plist	 	= $('#portfolio-list');
-	var $pfilter 		= $('#portfolio-filter');
-		
-	// Run Isotope  
-	$plist.isotope({
-		filter				: '*',
-		layoutMode   		: 'masonry',
-		animationOptions	: {
-		duration			: 750,
-		easing				: 'linear'
-	   }
-	});	
-	
-	// Isotope Filter 
-	$pfilter.find('a').click(function(){
-	  var selector = $(this).attr('data-filter');
-		$plist.isotope({ 
-		filter				: selector,
-		animationOptions	: {
-		duration			: 750,
-		easing				: 'linear',
-		queue				: false,
-	   }
-	  });
-	  return false;
-	});	 
-});	
+$(window).on("load", function () {
+  let loader = document.querySelector(".loader-container");
+  console.log("loader", loader);
+  setTimeout(() => {
+    loader.classList.add("hide-loader");
+  }, 1000);
+  setTimeout(() => {
+    loader.classList.add("display-none-loader");
+  }, 2000);
+});
